962c5ed088d089118cc35b0cdf300cf128e74c82
Alex <sascha.uchiha@yandex.ru>
2017-03-16 22:21:59 +0300

    Create GUI for Work via Academic Plans

---
+++ SomeDataBaseEditor/SomeDataBaseEditor/GUI DataTables Сatalogs/Working/DisciplineInAcademicPlan.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SomeDataBaseEditor.GUI_DataTables_Сatalogs.Working
+{
+    public partial class DisciplineInAcademicPlan : Form
+    {
+        public DisciplineInAcademicPlan()
+        {
+            InitializeComponent();
+        }
+
+        private void numericUpDown2_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
